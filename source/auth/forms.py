b2563878635808ebe32a295cbb3bdd7811f3a489
ark948 <armankarimi948@gmail.com>
2024-03-31 22:53:09 +0330

    Some code refactoring and template improvement

--- source/auth/forms.py
+++ source/auth/forms.py
@@ -1,25 +1,25 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, EmailField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, EqualTo, Email
+from wtforms.validators import DataRequired, EqualTo
 
 class RegisterForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    email = EmailField('Email', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired(), EqualTo('confirm')])
-    confirm = PasswordField('Confirm', validators=[DataRequired()])
-    submit = SubmitField('Submit')
+    username = StringField('نام کاربری', validators=[DataRequired()])
+    email = EmailField('آدرس ایمیل', validators=[DataRequired()])
+    password = PasswordField('رمزعبور', validators=[DataRequired(), EqualTo('confirm')])
+    confirm = PasswordField('تکرار رمزعبور', validators=[DataRequired()])
+    submit = SubmitField('ثبت نام')
 
 class LoginForm(FlaskForm):
-    username = StringField('Usernanme', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Login')
+    username = StringField('نام کاربری', validators=[DataRequired()])
+    password = PasswordField('رمزعبور', validators=[DataRequired()])
+    remember_me = BooleanField('مرا بخاطر بسپار')
+    submit = SubmitField('ورود')
 
 class ResetPasswordRequestForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired()])
-    submit = SubmitField('Request Password Reset')
+    email = EmailField('آدرس ایمیل', validators=[DataRequired()])
+    submit = SubmitField('درخواست تغییر رمزعبور')
 
 class ResetPasswordForm(FlaskForm):
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm = PasswordField('Confirm', validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Request Password Reset')
+    password = PasswordField('رمزعبور جدید', validators=[DataRequired()])
+    confirm = PasswordField('تکرار رمز', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('ثبت')
