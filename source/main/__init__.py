4c89eead5d5fe3d9ac37755265ecd17a09593012
ark948 <armankarimi948@gmail.com>
2024-04-07 15:59:49 +0330

    Added is_admin filter to main

--- source/main/__init__.py
+++ source/main/__init__.py
@@ -2,4 +2,4 @@ from flask import Blueprint
 
 bp = Blueprint('main', __name__)
 
-from source.main import routes
+from source.main import routes, utils
--- source/main/utils.py
+++ source/main/utils.py
@@ -1,5 +1,10 @@
 from source.main import bp
+from source.models.user import User
+from flask_login import current_user
 
 @bp.add_app_template_filter
-def is_admin_filter(current_user):
-    return current_user.is_admin
+def is_admin_filter(u):
+    if u.is_authenticated:
+        return u.is_admin()
+    else:
+        return False
